85f5539a9d36d92ceec413c7e75f0c08d22b7b7a
Victor Nunes <vlima-nu@student.42sp.org.br>
2023-12-28 21:15:57 -0300

    Desafio concluído

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,47 +1,96 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
+    public interface IValidadorDePlacas
+    {
+        public string FormatoPlaca { get; }
+
+        bool PlacaEhValida(string placa);
+    }
+
+    public class ValidadorDePlacasBr : IValidadorDePlacas
+    {
+        public string FormatoPlaca => "ABC-1D23 | ABC-1234";
+
+        private readonly Regex regexPlaca =
+            new(@"^[A-Z]{3}-[0-9][A-Z0-9][0-9]{2}$");
+
+        public bool PlacaEhValida(string placa)
+        {
+            if (string.IsNullOrEmpty(placa)) {
+                return false;
+            }
+            return regexPlaca.IsMatch(placa);
+        }
+    }
+
     public class Estacionamento
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private readonly decimal precoInicial;
+        private readonly decimal precoPorHora;
+        private readonly List<string> veiculos = new();
+        private readonly IValidadorDePlacas validadorDePlaca;
+
+        public Estacionamento(
+            decimal precoInicial,
+            decimal precoPorHora,
+            IValidadorDePlacas validadorDePlaca
+        )
+        {
+            if (precoInicial < 0 || precoPorHora < 0) {
+                throw new ArgumentException("Preços não podem ser negativos");
+            }
+            if (validadorDePlaca == null) {
+                throw new ArgumentNullException(nameof(validadorDePlaca));
+            }
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+            this.validadorDePlaca = validadorDePlaca;
+        }
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
+            if (precoInicial < 0 || precoPorHora < 0) {
+                throw new ArgumentException("Preços não podem ser negativos");
+            }
             this.precoInicial = precoInicial;
             this.precoPorHora = precoPorHora;
+            this.validadorDePlaca = new ValidadorDePlacasBr();
         }
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string placa = LerPlacaDeCarro();
+
+            if (veiculos.Contains(placa)) {
+                Console.WriteLine("Veículo já está estacionado");
+            }
+            else {
+                veiculos.Add(placa);
+            }
         }
 
         public void RemoverVeiculo()
         {
+            if (!veiculos.Any()) {
+                Console.WriteLine("Não há veículos estacionados.");
+                return;
+            }
+            ListarVeiculos();
+
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = LerPlacaDeCarro();
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            if (veiculos.Contains(placa)) {
+                uint horasEstacionado = LerHorasEstacionado();
+                decimal valorTotal = CalcularValorEstacionamento(horasEstacionado);
 
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
-            {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                veiculos.Remove(placa);
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                Console.WriteLine($"O veículo {placa} foi removido.");
+                Console.WriteLine($"O preço total foi de {valorTotal:C}.");
             }
             else
             {
@@ -55,13 +104,50 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                veiculos.ForEach(Console.WriteLine);
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        private static uint LerHorasEstacionado()
+        {
+            string horasString;
+
+            while (true)
+            {
+                Console.WriteLine("Digite a quantidade de horas que o " +
+                                  "veículo permaneceu estacionado:");
+
+                horasString = Console.ReadLine();
+                if (uint.TryParse(horasString, out uint horas)) {
+                    return horas;
+                }
+                Console.Error.WriteLine("Valor de horas inválido.");
+            }
+        }
+
+        private string LerPlacaDeCarro()
+        {
+            string placa;
+
+            while (true)
+            {
+                placa = Console.ReadLine().ToUpper();
+
+                if (validadorDePlaca.PlacaEhValida(placa)) {
+                    return placa;
+                }
+                Console.Error.WriteLine("Formato de placa inválido.");
+                Console.WriteLine($"Formatos aceitos: {validadorDePlaca.FormatoPlaca}");
+                Console.WriteLine("Tente novamente: ");
+            }
+        }
+
+        private decimal CalcularValorEstacionamento(uint horasEstacionado) {
+            return precoInicial + (precoPorHora * horasEstacionado);
         }
     }
 }
